998f0d952e5de429207b786affd2f6113d10c7d4
anil4use <anil4use@gmail.com>
2023-06-04 19:33:51 +0530

    fixed

--- app.js
+++ app.js
@@ -1,40 +1,78 @@
-import express  from "express";
+// import express  from "express";
+// import Cousrse from "./routes/CourseRouter.js";
+// import UserRouter from "./routes/UserRouter.js";
+// import {ErrorMiddleware} from "./middlewares/Error.js";
+// import cookieParser from "cookie-parser";
+// import paymentRouter from "./routes/paymentRouter.js";
+// import OtherRouter from "./routes/OtherRouter.js";
+// import cor from 'cors'
+
+// const app =express();
+// app.use(express.json());
+// app.use(express.urlencoded({
+//     extended:true
+// }))
+// app.use(cookieParser());
+// app.get("/", (req, res) => {
+//     res.send("server is working");
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+  
+// })
+// app.use(
+//     cor({
+//         // origin:[process.env.FRONTEND_URL],
+//         origin: '*',
+//         methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+//         credentials:true,
+        
+
+//     }) 
+// )
+
+// app.use("/api/v1",Cousrse);
+// app.use("/api/v1",UserRouter);
+// app.use("/api/v1",paymentRouter);
+// app.use("/api/v1",OtherRouter)
+
+// export default app;
+// app.use(ErrorMiddleware)
+import express from "express";
 import Cousrse from "./routes/CourseRouter.js";
 import UserRouter from "./routes/UserRouter.js";
-import {ErrorMiddleware} from "./middlewares/Error.js";
+import { ErrorMiddleware } from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
 import paymentRouter from "./routes/paymentRouter.js";
 import OtherRouter from "./routes/OtherRouter.js";
-import cor from 'cors'
+import cors from 'cors';
 
-const app =express();
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
-    extended:true
-}))
+  extended: true
+}));
 app.use(cookieParser());
-app.get("/", (req, res) => {
-    res.send("server is working");
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-  
-})
-app.use(
-    cor({
-        // origin:[process.env.FRONTEND_URL],
-        origin:"http://127.0.0.1:3000",
-        methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-        credentials:true
-    })
-)
 
-app.use("/api/v1",Cousrse);
-app.use("/api/v1",UserRouter);
-app.use("/api/v1",paymentRouter);
-app.use("/api/v1",OtherRouter)
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+  credentials: true
+}));
+
+app.get("/", (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.send("server is working");
+});
+
+app.use("/api/v1", Cousrse);
+app.use("/api/v1", UserRouter);
+app.use("/api/v1", paymentRouter);
+app.use("/api/v1", OtherRouter);
+
+app.use(ErrorMiddleware);
 
 export default app;
-app.use(ErrorMiddleware)
--- utils/SendToken.js
+++ utils/SendToken.js
@@ -3,7 +3,7 @@ export const SendToken=(res,user,message,statusCode=200)=>{
     const Options={
         expires:new Date(Date.now()+15*24*60*60*1000),
         httpOnly:true,
-        // secure:true,
+        secure:true,
         sameSite:"none"
     }
     res.status(statusCode).cookie("token",token,Options).json({
