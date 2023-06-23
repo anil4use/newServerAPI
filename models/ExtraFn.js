d2f08e91d28af0df5591d934a07cf46c70080391
anil4use <anil4use@gmail.com>
2023-06-23 15:55:35 +0530

    d

--- models/ExtraFn.js
+++ models/ExtraFn.js
@@ -2,6 +2,8 @@ import mongoose, { Schema } from "mongoose";
 const UserSchema = new mongoose.Schema({
     comments:{
         type:String,
+        required: [true, "Please enter commets"],
+
     }, 
     CreateAT: {
         type: Date,
